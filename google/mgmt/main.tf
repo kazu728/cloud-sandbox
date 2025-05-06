31a97ebd0c76644c06558fee0b603f924a298b5f
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2025-05-06 10:25:46 +0700

    project creation

---
+++ google/mgmt/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.0"
+    }
+  }
+
+  required_version = ">= 1.0"
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+
+  default_labels = {
+    "owner"      = "kazu728"
+    "managed_by" = "terraform"
+  }
+}
+
+resource "google_project" "cloud-sandbox" {
+  org_id          = var.org_id
+  project_id      = var.project_name
+  name            = var.project_name
+  billing_account = var.billing_account
+
+  deletion_policy = "PREVENT"
+}
---
+++ google/mgmt/variables.tf
@@ -0,0 +1,33 @@
+variable "org_id" {
+  description = "The ID of the GCP organization"
+  type        = string
+  default     = ""
+}
+
+variable "project_id" {
+  description = "The ID of the GCP project"
+  type        = string
+}
+
+variable "project_name" {
+  description = "The name of the GCP project"
+  type        = string
+
+}
+
+variable "region" {
+  description = "The region where resources will be created"
+  type        = string
+  default     = "asia-northeast1"
+}
+
+variable "zone" {
+  description = "The zone where resources will be created"
+  type        = string
+  default     = "asia-northeast1-a"
+}
+
+variable "billing_account" {
+  description = "The billing account ID for the GCP project"
+  type        = string
+}
